31f68311f0ebe1d8e4a4e80b6ac859ead0a27b45
Thorpe7 <maxwell.thorpe7@gmail.com>
2023-05-16 20:34:37 -0500

    Model. Data archive, curation, & loading

--- README.md
+++
@@ -1,2 +0,0 @@
-# Capstone
-Detailed comparison of Traditional Machine Learning methods to Deep Learning models on biomedical time series forecasting.
---
+++ source/utils/file_count.py
@@ -0,0 +1,21 @@
+''' Checking the number of files in data directories'''
+from pathlib import Path
+
+def get_file_count(dir_path:str):
+    
+    count = 0
+    data_dir = Path(dir_path)
+    if data_dir.exists():
+        
+        for x in data_dir.iterdir():
+            if x.is_file():
+                count+=1
+            elif x.is_dir():
+                get_file_count(x)
+    print(count)
+
+
+if __name__ == "__main__":
+    user_input = input("Provide file path here: ")
+    get_file_count(user_input)
+
